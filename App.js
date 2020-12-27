d153310f6d7c36072e5ab440b79d5a1f7f0aeede
Ivy Chen <ichen9@ucsc.edu>
2020-12-27 09:24:08 +0800

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, Image} from 'react-native';
+import Constants from 'expo-constants';
 
-export default function App() {
+const App = () =>{
+  console.log([Constants], Constants);
+  const {deviceName} = Constants;
+  function showAlert(){ 
+    console.log("Clicked!")
+    // alert("Clicked!")
+    Alert.alert(
+      "My Books",
+      "Your book has been selected",
+      [
+        {
+          text: "OK",
+          onPress: () => console.log("Pressed"),
+          
+        }
+      ]
+    )
+  }
+
+  
+   //JSX
   return (
-    <View style={styles.container}>
+    <View style = {styles.block}>
+      <Image 
+        style ={styles.img}
+        source = {require("./assets/image1.png")}
+      />
+      <Text style = {styles.title}>
+        Hello {deviceName}
+      </Text>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button 
+        onPress = {showAlert}
+        title = "Learn more"
+      
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  img: {
+    width: 120, 
+    height: 120,
+
   },
-});
+  block: {
+    paddingTop: 50, 
+    backgroundColor: "white",
+    flex: 1, //change the background from the size of text to the entie screen 
+    alignItems: "center", // align horizontally 
+    justifyContent: "center"
+  }, 
+  title: {
+    fontSize: 30,
+    color: "blue"
+  }
+})
+export default App; 
+
